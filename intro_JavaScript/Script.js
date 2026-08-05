af0c8f7c66e0770ef79e82937e7fc4f5dca82566
RAFAEL <rafaellimapapa898@gmail.com>
2026-08-05 17:40:41 -0300

    learn: hoist

--- intro_JavaScript/Script.js
+++ intro_JavaScript/Script.js
@@ -1,22 +1,39 @@
+//hoist: içamento 
+
+var idade = 18
+console.log("Oi, " + nome)
+if(idade >= 18){
+    var nome = "João"
+    console.log("Parabens, " + nome + ". Você pode dirigir")
+}
+console.log("Até mais, " + nome)
+/*
+
+let linguagem = "Javascript"
+console.log("Aprendendo " + linguagem)
+let linguagem = "Java"
+console.log("Aprendendo " + linguagem)
+
 var nome = "José"
 console.log(nome)
 nome = "João"
 console.log(nome)
 
-//let nome = "Ana"
-//console.log(nome)
-//nome = "Ana Maria"
-//console.log(nome)
+let nome = "Ana"
+console.log(nome)
+nome = "Ana Maria"
+console.log(nome)
 
-//começando...
-//const, let, var
-// a = 2
-// console.log(typeof(a))
-// a = "abc"
-// console. log(typeof(a))
-// a.falar()
-//
-//
-// int a = 2;
-// a = "abc";
-// a.falar();
+começando...
+const, let, var
+a = 2
+console.log(typeof(a))
+a = "abc"
+console. log(typeof(a))
+a.falar()
+
+
+int a = 2;
+a = "abc";
+a.falar();
+*/
